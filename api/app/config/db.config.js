0fe081c5fcebf16eb207f96979c01e8c38f8d8c5
elfarsif <f.n.elfarsi@gmail.com>
2024-02-25 14:32:43 -0500

    Initial commit

---
+++ api/app/config/db.config.js
@@ -0,0 +1,6 @@
+module.exports = {
+  MONGODB_CONNECTION_STRING:"mongodb+srv://elfarsif:EwPidXuAvYrTl1zD@todocluster.dwmwd6u.mongodb.net/login?retryWrites=true&w=majority",
+  HOST: "localhost",
+  PORT: 27017,
+  DB: "bezkoder_db"
+};
---
+++ api/server.js
@@ -0,0 +1,77 @@
+const express = require("express");
+const cors = require("cors");
+const cookieSession = require("cookie-session");
+const dbConfig = require("./app/config/db.config");
+
+const app = express();
+
+const corsOptions = {
+  origin: "http://localhost:4200",
+  credentials: true  // Allow cookies, authorization headers, etc. to be included in requests
+};
+
+app.use(cors(corsOptions));
+// parse requests of content-type - application/json
+app.use(express.json());
+
+// parse requests of content-type - application/x-www-form-urlencoded
+app.use(express.urlencoded({ extended: true }));
+
+app.use(
+  cookieSession({
+    name: "bezkoder-session",
+    keys: ["COOKIE_SECRET"], // should use as secret environment variable
+    httpOnly: true
+  })
+);
+
+const db = require("./app/models");
+const Role = db.role;
+
+db.mongoose
+  .connect(`${dbConfig.MONGODB_CONNECTION_STRING}`, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+  })
+  .then(() => {
+    console.log("Successfully connect to MongoDB.");
+    initial();
+  })
+  .catch(err => {
+    console.error("Connection error", err);
+    process.exit();
+  });
+
+// simple route
+app.get("/", (req, res) => {
+  res.json({ message: "Welcome to bezkoder application." });
+});
+
+// routes
+require('./app/routes/auth.routes')(app);
+require('./app/routes/user.routes')(app);
+
+
+// set port, listen for requests
+const PORT = process.env.PORT || 8080;
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}.`);
+});
+
+async function initial() {
+  try {
+    const count = await Role.estimatedDocumentCount();
+    if (count === 0) {
+      await Promise.all([
+        new Role({ name: "user" }).save(),
+        new Role({ name: "moderator" }).save(),
+        new Role({ name: "admin" }).save()
+      ]);
+      console.log("Roles added successfully.");
+    } else {
+      console.log("Roles already exist.");
+    }
+  } catch (err) {
+    console.error("Error initializing roles:", err);
+  }
+}
